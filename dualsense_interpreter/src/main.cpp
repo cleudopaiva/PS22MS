94f018a4f249bbdb0b0b6031074907f9946050f0
Cleudo Paiva <cleudojunior121618@gmail.com>
2026-07-21 17:03:28 -0300

    chore: make clangd support

--- dualsense_interpreter/src/main.cpp
+++ dualsense_interpreter/src/main.cpp
@@ -1,4 +1,6 @@
 #include <array>
+#include <cerrno>
+#include <cstring>
 #include <hidapi/hidapi.h>
 #include <iostream>
 
@@ -6,18 +8,24 @@ constexpr unsigned short SONY_VID = 0x054C;
 constexpr unsigned short DUALSENSE_PID = 0x0CE6;
 
 int main() {
-  if (hid_init() != 0) {
-    std::cerr << "Error initializing HIDAPI\n";
-    return 1;
-  }
-
   hid_device *controller = hid_open(SONY_VID, DUALSENSE_PID, nullptr);
 
   if (controller == nullptr) {
-    std::cerr << "Dualsense not found or without permition\n";
+    int err = errno;
+
+    const wchar_t *hid_err = hid_error(nullptr);
+
+    if (hid_err) {
+      std::wcerr << L"hid_error:" << hid_err << L'\n';
+    }
+
+    std::cerr << "errno: " << strerror(err) << '\n';
+
     hid_exit();
     return 1;
   }
 
   std::array<unsigned char, 128> report{};
+
+  return 0;
 }
